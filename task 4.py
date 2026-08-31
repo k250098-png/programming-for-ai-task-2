d24b539cf22a9786110a52c41f41dbc78af0d8f0
Hasan Akram <k250098@nu.edu.pk>
2026-08-31 12:02:50 +0500

    Create task 4.py

---
+++ task 4.py
@@ -0,0 +1,19 @@
+
+course_a = {"S101", "S102", "S103", "S104", "S105"}
+course_b = {"S103", "S104", "S106", "S107"}
+
+both_courses = course_a.intersection(course_b)
+
+
+only_a = course_a.difference(course_b)
+
+only_b = course_b.difference(course_a)
+
+
+all_students = course_a.union(course_b)
+
+print("--- Enrollment Analysis ---")
+print("Enrolled in both courses:", both_courses)
+print("Enrolled only in Course A:", only_a)
+print("Enrolled only in Course B:", only_b)
+print("All unique students:", all_students)
