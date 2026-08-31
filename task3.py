4567d3f6002c86847066ddd1ba0e0dcc08a756be
Hasan Akram <k250098@nu.edu.pk>
2026-08-31 11:58:37 +0500

    Implement transaction processing with duplicates detection

---
+++ task3.py
@@ -0,0 +1,20 @@
+transactions = [
+    "TXN1001", "TXN1002", "TXN1001", "TXN1003", 
+    "TXN1004", "TXN1002", "TXN1005", "TXN1001"
+]
+
+seen = set()
+duplicates = set()
+
+
+for tx in transactions:
+    if tx in seen:
+        duplicates.add(tx)
+    else:
+        seen.add(tx)
+
+unique_transactions = set(transactions)
+
+print("--- Transaction Processing ---")
+print("Unique Transactions:", list(unique_transactions))
+print("Duplicate Transactions Identified:", list(duplicates))
